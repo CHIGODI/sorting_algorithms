df7eb41c9e6c3853811f7b5149076eea47c5abaf
CHIGODI <chiegody254@gmail.com>
2024-01-19 10:16:22 +0300

    Betty correction

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,35 +8,31 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-        listint_t *temp, *loop;
+	listint_t *temp, *loop;
 
-        temp = (*list)->next;
-
+	temp = (*list)->next;
 	print_list(*list);
+	while (temp != NULL)
+	{
+		loop = temp->prev;
+		while (loop != NULL && loop->n > temp->n)
+		{
+			if (loop->prev != NULL)
+				loop->prev->next = temp;
+			if (temp->next != NULL)
+				temp->next->prev = loop;
 
-        while(temp != NULL)
-        {
-                loop = temp->prev;
-                while(loop != NULL && loop->n > temp->n)
-                {
-                        if (loop->prev != NULL)
-                                loop->prev->next = temp;
-                        if (temp->next != NULL)
-                                temp->next->prev = loop;
-
-                        loop->next = temp->next;
-                        temp->prev = loop->prev;
-                        loop->prev = temp;
-                        temp->next = loop;
-
-                        if (temp->prev == NULL)
-                        {
-                                *list = temp;
-                        }
-                        loop = temp->prev;
-
+			loop->next = temp->next;
+			temp->prev = loop->prev;
+			loop->prev = temp;
+			temp->next = loop;
+			if (temp->prev == NULL)
+			{
+				*list = temp;
+			}
+			loop = temp->prev;
 			print_list(*list);
-                }
-                temp = temp->next;
-        }
+		}
+		temp = temp->next;
+	}
 }
